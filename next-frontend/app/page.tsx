fdd6fa5c1bfae4b164b3bd6c9610934d38eceaf3
Eli Song <eli.ehnj2@gmail.com>
2026-01-19 16:29:05 -0500

    fix

--- next-frontend/app/page.tsx
+++ next-frontend/app/page.tsx
@@ -129,7 +129,7 @@ export default function Home() {
       <h1 className="text-2xl font-bold mb-4 flex flex-col items-center mt-50">
         Welcome to liwe.gg!
       </h1>
-      <div className="min-h-screen flex flex-col items-center justify-center -mt-65">
+      <div className="min-h-screen flex flex-col items-center justify-center">
         <p>enter mingoose9#NA1 to test</p>
         <Searchbar onSearch={handleSearch}></Searchbar>
       </div>
