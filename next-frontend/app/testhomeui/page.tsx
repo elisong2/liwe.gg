6858c934af8c29ada3987d6229bef42052b1990f
Eli Song <eli.ehnj2@gmail.com>
2026-02-15 21:33:09 -0500

    fixing eslint

--- next-frontend/app/testhomeui/page.tsx
+++ next-frontend/app/testhomeui/page.tsx
@@ -38,47 +38,6 @@ export default function testui() {
       <p className="text-center pt-5 font-italic">
         League stats you didn't know about yourself!
       </p>
-
-      <div className="mt-15 justify-center items-center flex flex-col ">
-        <Searchbar onSearch={handleSearch}></Searchbar>
-      </div>
-
-      <div className="justify-center items-center flex mt-10">
-        <Tabs defaultValue="overview" className="w-[400px]">
-          <TabsList className="grid w-full grid-cols-3">
-            <TabsTrigger value="overview">Overview</TabsTrigger>
-            <TabsTrigger value="matches">Match History</TabsTrigger>
-            <TabsTrigger value="reports">Reports</TabsTrigger>
-          </TabsList>
-          <TabsContent value="overview">
-            <Card>
-              <CardHeader>
-                <CardTitle>Overview</CardTitle>
-                <CardDescription>
-                  View your key metrics and recent project activity. Track
-                  progress across all your active projects.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="text-muted-foreground text-sm">
-                You have 12 active projects and 3 pending tasks.
-              </CardContent>
-            </Card>
-          </TabsContent>
-          <TabsContent value="matches">
-            <Card>
-              <CardHeader>
-                <CardTitle>Match History</CardTitle>
-                <CardDescription></CardDescription>
-              </CardHeader>
-              <CardContent className="text-muted-foreground text-sm"></CardContent>
-            </Card>
-          </TabsContent>
-        </Tabs>
-      </div>
-
-      <p className="text-center text-sm bottom-10 absolute w-full">
-        made with FastAPI + Next.js
-      </p>
     </>
   );
 }
